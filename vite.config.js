b381a110b89d48192f3229517b8d21ec3774f95b
Michael Angamarca <michaelangamarca558@gmail.com>
2024-09-21 00:26:21 -0400

    all

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/ma-portfolio/",
+  plugins: [react()],
+  css:{
+    modules:{
+      localsConvention: "camelCase",
+    }
+  },
+  // build:{
+  //   outDir: 'build'
+  // }
+})
